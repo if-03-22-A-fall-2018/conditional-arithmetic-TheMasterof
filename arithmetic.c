26e196e07f3602bb00866c1a4ad5708b62580654
Marcel Plakolb <marcel.plakolb@liwest.at>
2018-10-16 11:29:34 +0200

    Fixed logical Errors

--- arithmetic.c
+++ arithmetic.c
@@ -1,29 +1,35 @@
-#include <stdio.h>
-#include <Methods.h>
+#include "stdio.h"
+#include "stdbool.h"
+#include "Methods.h"
 
 int main(int argc, char const *argv[]) {
   int choice;
-
+choice=Choice(choice);
   return 0;
 }
 int Choice(int choice)
 {
   do {
-  printf("Menu\n", );
-  printf("=========\n", );
-  printf("1.) Addieren\n", );
-  printf("2.) Multiplizieren\n", );
-  printf("======================\n", );
-  printf("Enter your choice: ", );
-  scanf("%d\n"choice );
-  if (!Check(choice)) {
-    printf("\nInvalid Input!\n", );
+  printf("Menu\n");
+  printf("=========\n");
+  printf("1.) Addieren\n");
+  printf("2.) Multiplizieren\n");
+  printf("======================\n");
+  printf("Enter your choice: ");
+  scanf("%d",&choice);
+  if (Check(choice)) {
+    printf("\nInvalid Input!\n");
   }
-}while(!Check(choice));
+}while(Check(choice)==true);
 
-
+return choice;
 }
+
 bool Check(int choice){
-  return choice==1||choice==2;
+  if (choice==1||choice==2) {
+  return false;
+  }
+else{
+  return true;
   }
 }
